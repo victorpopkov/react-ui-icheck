2d4fc4f162cd76de385246a7097d74d6f1b42460
Victor Popkov <victor@popkov.me>
2018-10-11 10:21:04 +0300

    Add website webpack configs

---
+++ website/webpack/webpack.config.dev.js
@@ -0,0 +1,116 @@
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const common = require('./webpack.config.common');
+const config = require('../config');
+const paths = require('./paths');
+
+module.exports = merge(common, {
+  devServer: {
+    contentBase: paths.websiteDist,
+    host: config.appHost,
+    hot: true,
+    hotOnly: true,
+    inline: true,
+    noInfo: true,
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+    port: config.appPort,
+    publicPath: common.output.publicPath,
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'eslint-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              localIdentName: '[local]___[hash:base64:5]',
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              localIdentName: '[local]___[hash:base64:5]',
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [],
+              outputStyle: 'expanded',
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.IgnorePlugin(/webpack-stats\.json$/),
+    new StyleLintPlugin(),
+  ],
+});
---
+++ website/webpack/webpack.config.prod.js
@@ -0,0 +1,126 @@
+const CleanPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const common = require('./webpack.config.common');
+const paths = require('./paths');
+
+module.exports = merge(common, {
+  devtool: 'hidden-source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              localIdentName: '[local]___[hash:base64:5]',
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              localIdentName: '[local]___[hash:base64:5]',
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+            warnings: false,
+          },
+        },
+        sourceMap: true,
+      }),
+      new OptimizeCssAssetsPlugin({
+        canPrint: false,
+        cssProcessor: require('cssnano'), // eslint-disable-line global-require
+        cssProcessorOptions: {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      }),
+    ],
+  },
+  output: {
+    filename: '[name]-[hash].js',
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/\/config$/, /\.\/dev/),
+    new CleanPlugin([paths.websiteDist], {
+      root: paths.root,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name]-[chunkhash].css',
+      chunkFilename: 'assets/css/[id].css',
+    }),
+  ],
+});
