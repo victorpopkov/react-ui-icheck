24fa99f63eefd6b747ecefef7f9f2d9180700c99
Victor Popkov <victor@popkov.me>
2018-10-12 13:14:12 +0300

    Refactor website app installation

--- website/src/app/installation/Installation.js
+++ website/src/app/installation/Installation.js
@@ -1,51 +1,56 @@
-import { Col, Row } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { Jumbotron, Sidebar } from '../common'; // eslint-disable-line sort-imports
 
 const Installation = () => (
-  <div className="installation">
-    <h2 className="mb-4">Installation</h2>
-    <Row>
-      <Col xs={12}>
-        <p>
-          You can install this library as any other
-          {' '}
-          <a href="https://www.npmjs.com/" rel="noopener noreferrer" target="_blank">npm</a>
-          {' '}
-          package.
-        </p>
-        <p>
-          If you are planning to use your own custom skin, then only the package of this library
-          is needed:
-        </p>
-        <pre className="mb-3">
-          <code className="language-bash">
-            {`# using npm
+  <main role="main">
+    <Jumbotron />
+    <Container>
+      <Row>
+        <Col md={9}>
+          <h2 className="mb-4">Installation</h2>
+          <p>
+            You can install this library as any other
+            {' '}
+            <a href="https://www.npmjs.com/" rel="noopener noreferrer" target="_blank">npm</a>
+            {' '}
+            package.
+          </p>
+          <p>
+            If you are planning to use your own custom skin, then only the package of this library
+            is needed:
+          </p>
+          <pre className="mb-3">
+            <code className="language-bash">
+              {`# using npm
 npm install react-ui-icheck --save
 
 # using Yarn
 yarn add react-ui-icheck`}
-          </code>
-        </pre>
-        <p>
-          If you are planning to use the original
-          {' '}
-          <a href="https://github.com/fronteed/icheck" rel="noopener noreferrer" target="_blank">iCheck</a>
-          {' '}
-          skins, then you should install both packages:
-        </p>
-        <pre>
-          <code className="language-bash">
-            {`# using npm
+            </code>
+          </pre>
+          <p>
+            If you are planning to use the original
+            {' '}
+            <a href="https://github.com/fronteed/icheck" rel="noopener noreferrer" target="_blank">iCheck</a>
+            {' '}
+            skins, then you should install both packages:
+          </p>
+          <pre>
+            <code className="language-bash">
+              {`# using npm
 npm install react-ui-icheck icheck --save
 
 # using Yarn
 yarn add react-ui-icheck icheck`}
-          </code>
-        </pre>
-      </Col>
-    </Row>
-  </div>
+            </code>
+          </pre>
+        </Col>
+        <Sidebar />
+      </Row>
+    </Container>
+  </main>
 );
 
 export default hot(module)(Installation);
