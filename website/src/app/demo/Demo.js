8e8cb2015923444c4e55dd66ad781a4005f5638f
Victor Popkov <victor@popkov.me>
2018-10-12 13:13:56 +0300

    Refactor website app demo

--- website/src/app/demo/Demo.js
+++ website/src/app/demo/Demo.js
@@ -1,14 +1,18 @@
 import {
+  Col,
+  Container,
   Nav,
   NavItem,
   NavLink,
+  Row,
   TabContent,
   TabPane,
 } from 'reactstrap';
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import { hot } from 'react-hot-loader';
-import DemoCheckbox from './DemoCheckbox'; // eslint-disable-line sort-imports
+import { Jumbotron, Sidebar } from '../common'; // eslint-disable-line sort-imports
+import DemoCheckbox from './DemoCheckbox';
 import DemoRadio from './DemoRadio';
 
 @hot(module)
@@ -37,38 +41,46 @@ class Demo extends Component {
     const { activeTab } = this.state;
 
     return (
-      <div className="demo">
-        <h2 className="mb-4">Demo</h2>
-        <Nav className="mt-4" tabs>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: activeTab === 'checkbox' })}
-              onClick={() => { this.toggleTab('checkbox'); }}
-            >
-              Checkbox
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classNames({ active: activeTab === 'radio' })}
-              onClick={() => { this.toggleTab('radio'); }}
-            >
-              Radio
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent
-          activeTab={activeTab}
-          className="mb-4"
-        >
-          <TabPane tabId="checkbox">
-            <DemoCheckbox />
-          </TabPane>
-          <TabPane tabId="radio">
-            <DemoRadio />
-          </TabPane>
-        </TabContent>
-      </div>
+      <main role="main">
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col md={9}>
+              <h2 className="mb-4">Demo</h2>
+              <Nav className="mt-4" tabs>
+                <NavItem>
+                  <NavLink
+                    className={classNames({ active: activeTab === 'checkbox' })}
+                    onClick={() => { this.toggleTab('checkbox'); }}
+                  >
+                    Checkbox
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={classNames({ active: activeTab === 'radio' })}
+                    onClick={() => { this.toggleTab('radio'); }}
+                  >
+                    Radio
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <TabContent
+                activeTab={activeTab}
+                className="mb-4"
+              >
+                <TabPane tabId="checkbox">
+                  <DemoCheckbox />
+                </TabPane>
+                <TabPane tabId="radio">
+                  <DemoRadio />
+                </TabPane>
+              </TabContent>
+            </Col>
+            <Sidebar />
+          </Row>
+        </Container>
+      </main>
     );
   }
 }
