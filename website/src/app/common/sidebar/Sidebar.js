f1f2798e8df5aa18720afa0a9368f1ec9ce6adeb
Victor Popkov <victor@popkov.me>
2018-10-12 13:07:43 +0300

    Add website app common jumbotron and sidebar

---
+++ website/src/app/common/sidebar/Sidebar.js
@@ -0,0 +1,70 @@
+import {
+  Col,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+import React from 'react';
+
+const Sidebar = () => (
+  <Col className="d-none d-md-block" md={3}>
+    <h5 className="pb-2">Getting Started</h5>
+    <Nav vertical>
+      <NavItem>
+        <IndexLinkContainer to="/">
+          <NavLink>Overview</NavLink>
+        </IndexLinkContainer>
+      </NavItem>
+      <NavItem>
+        <LinkContainer to="/demo">
+          <NavLink>Demo</NavLink>
+        </LinkContainer>
+      </NavItem>
+      <NavItem>
+        <LinkContainer to="/installation">
+          <NavLink>Installation</NavLink>
+        </LinkContainer>
+      </NavItem>
+    </Nav>
+    <hr className="mb-4" />
+    <h5 className="pb-2">Skins</h5>
+    <Nav vertical>
+      <NavItem>
+        <LinkContainer to="/skin/flat">
+          <NavLink>Flat</NavLink>
+        </LinkContainer>
+      </NavItem>
+      <NavItem>
+        <LinkContainer to="/skin/line">
+          <NavLink>Line</NavLink>
+        </LinkContainer>
+      </NavItem>
+      <NavItem>
+        <LinkContainer to="/skin/minimal">
+          <NavLink>Minimal</NavLink>
+        </LinkContainer>
+      </NavItem>
+      <NavItem>
+        <LinkContainer to="/skin/square">
+          <NavLink>Square</NavLink>
+        </LinkContainer>
+      </NavItem>
+    </Nav>
+    <hr />
+    <Nav vertical>
+      <NavItem>
+        <LinkContainer to="/skin/futurico">
+          <NavLink>Futurico</NavLink>
+        </LinkContainer>
+      </NavItem>
+      <NavItem>
+        <LinkContainer to="/skin/polaris">
+          <NavLink>Polaris</NavLink>
+        </LinkContainer>
+      </NavItem>
+    </Nav>
+  </Col>
+);
+
+export default Sidebar;
