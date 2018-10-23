57a35311b7f1b22d8d6165844fa375488b863b1c
Victor Popkov <victor@popkov.me>
2018-10-23 22:12:52 +0300

    Add CheckboxGroup tests

---
+++ src/__tests__/CheckboxGroup.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+import { Checkbox, CheckboxGroup } from '../index'; // eslint-disable-line sort-imports
+
+describe('<CheckboxGroup /> component', () => {
+  const testCheckboxGroup = (
+    <CheckboxGroup>
+      <Checkbox />
+    </CheckboxGroup>
+  );
+
+  it('should have the default props values when mounted', () => {
+    const wrapper = mount(testCheckboxGroup);
+    expect(wrapper.props()).toMatchSnapshot();
+  });
+
+  describe('should render the corresponding HTML', () => {
+    it('when only the default props are set', () => {
+      const wrapper = mount(testCheckboxGroup);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('when the checkboxWrapClassName and checkboxWrapTag props values are set', () => {
+      const wrapper = mount(
+        <CheckboxGroup checkboxWrapClassName="test" checkboxWrapTag="div">
+          <Checkbox />
+        </CheckboxGroup>
+      );
+
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
