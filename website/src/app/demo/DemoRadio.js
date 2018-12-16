ab291d382089862704db9a7752dca521c9ddaee5
Victor Popkov <victor@popkov.me>
2018-12-16 16:35:00 +0200

    Add "Properties" in the website DemoRadio

--- website/src/app/demo/DemoRadio.js
+++ website/src/app/demo/DemoRadio.js
@@ -4,6 +4,7 @@ import {
   Nav,
   NavItem,
   NavLink,
+  Row,
   TabContent,
   TabPane,
 } from 'reactstrap';
@@ -194,6 +195,64 @@ this.state = {
             </pre>
           </TabPane>
         </TabContent>
+        <h5 className="mb-4 mt-4">Properties</h5>
+        <Row>
+          <Col xs={12}>
+            <h6 className="mb-4 mt-4">CheckboxGroup</h6>
+            <pre>
+              <code className="language-jsx">
+                {`RadioGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  defaultValue: PropTypes.string,
+  radioWrapClassName: PropTypes.string,
+  radioWrapTag: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};`}
+              </code>
+            </pre>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <h6 className="mb-4 mt-4">Checkbox</h6>
+            <pre>
+              <code className="language-jsx">
+                {`Radio.propTypes = {
+  activeClass: PropTypes.string,        // default: 'active'
+  aria: PropTypes.bool,                 // default: false
+  checked: PropTypes.bool,              // default: false
+  checkedClass: PropTypes.string,       // default: 'checked'
+  children: PropTypes.node,
+  defaultChecked: PropTypes.bool,       // default: false
+  determinateClass: PropTypes.string,
+  disabled: PropTypes.bool,
+  disabledClass: PropTypes.string,      // default: 'disabled'
+  focusClass: PropTypes.string,         // default: 'focus'
+  hoverClass: PropTypes.string,         // default: 'hover'
+  increaseArea: PropTypes.string,
+  indeterminate: PropTypes.bool,        // default: true
+  indeterminateClass: PropTypes.string, // default: 'indeterminate'
+  inheritClass: PropTypes.bool,         // default: false
+  inheritID: PropTypes.bool,            // default: false
+  insert: PropTypes.node,
+  label: PropTypes.node,
+  labelClassName: PropTypes.string,
+  labelHover: PropTypes.bool,           // default: true
+  labelHoverClass: PropTypes.string,    // default: 'hover'
+  labelTag: PropTypes.string,           // default: 'span'
+  labelTagClassName: PropTypes.string,
+  radioClass: PropTypes.string,         // default: 'iradio'
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+};`}
+              </code>
+            </pre>
+          </Col>
+        </Row>
       </Col>
     );
   }
