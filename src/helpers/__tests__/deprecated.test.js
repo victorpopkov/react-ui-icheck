1bab9fd9be97be3fdec6f437b4c3a82c30a9b0f1
Victor Popkov <victor@popkov.me>
2018-12-18 21:40:14 +0200

    Add deprecated helper tests

---
+++ src/helpers/__tests__/deprecated.test.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+import deprecated from '../deprecated'; // eslint-disable-line sort-imports
+
+describe('deprecated helper', () => {
+  const TestComponent = () => null;
+
+  TestComponent.propTypes = {
+    testProp: PropTypes.string,
+    testPropDeprecated: deprecated(PropTypes.string, 'newProp'),
+  };
+
+  beforeEach(() => {
+    console.warn = jest.fn();
+  });
+
+  describe('when mounting a React component', () => {
+    it('shouldn\'t throw a console.warn when not using a deprecated property', () => {
+      mount(<TestComponent testProp="test" />);
+      expect(console.warn).toHaveBeenCalledTimes(0);
+    });
+
+    it('should throw a console.warn when using a deprecated property', () => {
+      mount(<TestComponent testPropDeprecated="test" />);
+      expect(console.warn).toHaveBeenCalledTimes(1);
+    });
+  });
+});
