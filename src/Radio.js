a25dd1c4c037f06335dc0c534a4593e536d08819
Victor Popkov <victor@popkov.me>
2019-05-31 15:03:57 +0300

    Reuse BaseInput renderInput() in Radio

--- src/Radio.js
+++ src/Radio.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
 import BaseInput from './BaseInput'; // eslint-disable-line sort-imports
-import Input from './components/Input';
 import deprecated from './helpers/deprecated';
 
 class Radio extends BaseInput {
@@ -18,89 +16,12 @@ class Radio extends BaseInput {
   };
 
   renderInput() {
-    const {
-      activeClass,
-      activeClassName,
-      aria,
-      checkedClass,
-      checkedClassName,
-      className,
-      defaultChecked,
-      disabled,
-      disabledClass,
-      disabledClassName,
-      focusClass,
-      focusClassName,
-      hoverClass,
-      hoverClassName,
-      id,
-      increaseArea,
-      indeterminate,
-      indeterminateClass,
-      indeterminateClassName,
-      inheritClass,
-      inheritClassName,
-      inheritID,
-      insert,
-      name,
-      onBlur,
-      onChange,
-      onFocus,
-      radioClass,
-      radioClassName,
-      value,
+    const { radioClass, radioClassName } = this.props;
 
-      checked,
-      children,
-      label,
-      labelClassName,
-      labelHover,
-      labelHoverClass,
-      labelTag,
-      labelTagClassName,
-      onMouseDown,
-      onMouseLeave,
-      onMouseUp,
-      onTouchEnd,
-      onTouchStart,
-      type,
-
-      ...other
-    } = this.props;
-    const { checked: stateChecked, hovered, isMobile } = this.state;
-
-    return (
-      <Input
-        {...other}
-        activeClassName={activeClass || activeClassName}
-        aria={aria}
-        checked={stateChecked}
-        checkedClassName={checkedClass || checkedClassName}
-        className={className}
-        defaultChecked={defaultChecked}
-        disabled={disabled}
-        disabledClassName={disabledClass || disabledClassName}
-        focusClassName={focusClass || focusClassName}
-        hoverClassName={hoverClass || hoverClassName}
-        hovered={hovered}
-        id={id}
-        increaseArea={increaseArea}
-        indeterminate={indeterminate}
-        indeterminateClassName={indeterminateClass || indeterminateClassName}
-        inheritClassName={inheritClass || inheritClassName}
-        inheritID={inheritID}
-        inputType="radio"
-        insert={insert}
-        isMobile={isMobile}
-        name={name}
-        radioClassName={radioClass || radioClassName}
-        ref={(c) => { this.input = c; }}
-        value={value}
-        onBlur={onBlur}
-        onChange={onChange}
-        onFocus={onFocus}
-      />
-    );
+    return super.renderInput({
+      inputType: 'radio',
+      radioClassName: radioClass || radioClassName,
+    });
   }
 }
 
