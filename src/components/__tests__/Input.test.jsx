4e833e3b247c42a16dfb03315ba53951322a3cb5
Victor Popkov <victor@popkov.me>
2020-06-18 13:40:57 +0300

    Refactor Input component tests

--- src/components/__tests__/Input.test.jsx
+++ src/components/__tests__/Input.test.jsx
@@ -4,212 +4,351 @@ import expect from 'expect';
 import Input from '../Input'; // eslint-disable-line sort-imports
 
 const testInputComponent = (inputType) => {
-  const testInput = <Input inputType={inputType} />;
-  const testSpy = jest.spyOn(Input.prototype, 'componentWillReceiveProps');
+  let wrapper;
 
-  afterEach(() => {
-    testSpy.mockClear();
-  });
-
-  it('should have the default props values when mounted', () => {
-    const wrapper = mount(testInput);
-    expect(wrapper.props()).toMatchSnapshot();
-  });
-
-  it('should render the corresponding HTML when the insert value is set', () => {
-    const input = <Input inputType={inputType} insert={<div>test</div>} />;
-
-    const wrapper = render(input);
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  describe('should render wrapper', () => {
-    it('with corresponding className when inheritClassName is passed', () => {
-      const wrapper = shallow(
-        <Input className="test" inputType={inputType} inheritClassName />,
-      );
-      expect(wrapper.props().className).toBe(`i${inputType} test`);
+  describe('as mount', () => {
+    beforeEach(() => {
+      wrapper = mount(<Input inputType={inputType} />);
     });
 
-    it('with corresponding id when inheritID is passed', () => {
-      const wrapper = shallow(
-        <Input id="test" inputType={inputType} inheritID />,
-      );
-      expect(wrapper.props().id).toBe('iCheck-test');
+    it('should match the default props snapshot', () => {
+      expect(wrapper.props()).toMatchSnapshot();
     });
 
-    describe('with aria labels ', () => {
-      const testAriaInput = <Input inputType={inputType} aria />;
-
-      it('when aria prop is passed', () => {
-        const wrapper = shallow(testAriaInput);
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('when aria prop is passed and the checked state changes', () => {
-        const wrapper = shallow(testAriaInput);
-
-        expect(wrapper.props()['aria-checked']).toBe(false);
-        wrapper.setState({ checked: true });
-        expect(wrapper.props()['aria-checked']).toBe(true);
-      });
-    });
-  });
-
-  describe('should render the <input />', () => {
-    it('on mobiles', () => {
-      const wrapper = shallow(testInput);
-      wrapper.setProps({ isMobile: true });
-      expect(wrapper.find('input').prop('style')).toMatchSnapshot();
+    it('should match the default state snapshot', () => {
+      expect(wrapper.state()).toMatchSnapshot();
     });
 
-    describe('when increaseArea is passed', () => {
-      it('as a positive value', () => {
-        const wrapper = shallow(
-          <Input increaseArea="30%" inputType={inputType} />,
-        );
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('as a negative value', () => {
-        const wrapper = shallow(
-          <Input increaseArea="-30%" inputType={inputType} />,
-        );
-        expect(wrapper.find('input').props()).toMatchSnapshot();
-      });
-
-      it('as a negative value and more than 50%', () => {
-        const wrapper = shallow(
-          <Input increaseArea="-60%" inputType={inputType} />,
-        );
-        expect(wrapper).toMatchSnapshot();
-      });
-    });
-  });
-
-  describe('should update the state', () => {
-    it('when the checked prop has changed', () => {
-      const wrapper = mount(testInput);
-
-      expect(testSpy).toHaveBeenCalledTimes(0);
-      expect(wrapper.props().checked).toBe(false);
+    it('should have the `checked` state as `false`', () => {
       expect(wrapper.state().checked).toBe(false);
-
-      wrapper.setProps({ checked: true });
-
-      expect(testSpy).toHaveBeenCalledTimes(1);
-      expect(wrapper.props().checked).toBe(true);
-      expect(wrapper.state().checked).toBe(true);
     });
 
-    it('when the hovered prop has changed', () => {
-      const wrapper = mount(testInput);
-
-      expect(testSpy).toHaveBeenCalledTimes(0);
-      expect(wrapper.props().hovered).toBe(false);
+    it('should have the `hovered` state as `false`', () => {
       expect(wrapper.state().hovered).toBe(false);
-
-      wrapper.setProps({ hovered: true });
-
-      expect(testSpy).toHaveBeenCalledTimes(1);
-      expect(wrapper.props().hovered).toBe(true);
-      expect(wrapper.state().hovered).toBe(true);
-    });
-  });
-
-  describe('should handle the <input />', () => {
-    it('onFocus()', () => {
-      const spy = jest.fn();
-      const wrapper = mount(<Input inputType={inputType} onFocus={spy} />);
-
-      expect(spy).toHaveBeenCalledTimes(0);
-      expect(wrapper.state().focused).toBe(false);
-
-      wrapper.find('input').simulate('focus');
-
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(wrapper.state().focused).toBe(true);
     });
 
-    it('onBlur()', () => {
-      const spy = jest.fn();
-      const wrapper = mount(<Input inputType={inputType} onBlur={spy} />);
+    describe('but the `checked` prop has changed from `false` to `true`', () => {
+      beforeEach(() => {
+        wrapper.setProps({ checked: true });
+      });
 
-      wrapper.setState({ focused: true });
-
-      expect(spy).toHaveBeenCalledTimes(0);
-      expect(wrapper.state().focused).toBe(true);
-
-      wrapper.find('input').simulate('blur');
-
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(wrapper.state().focused).toBe(false);
-    });
-
-    it('onChange()', () => {
-      const spy = jest.fn();
-      const wrapper = mount(<Input inputType={inputType} onChange={spy} />);
-
-      expect(spy).toHaveBeenCalledTimes(0);
-      expect(wrapper.state().checked).toBe(false);
-
-      wrapper.find('input').simulate('change');
-
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(wrapper.state().checked).toBe(true);
-    });
-  });
-
-  describe('should handle the <ins />', () => {
-    it('onClick()', () => {
-      const spy = jest.fn();
-      const wrapper = mount(
-        <Input
-          inputType={inputType}
-          insert="Insert"
-          label="Label"
-          onChange={spy}
-        />,
-      );
-
-      expect(spy).toHaveBeenCalledTimes(0);
-      expect(wrapper.state().checked).toBe(false);
-
-      wrapper.find('ins').simulate('click');
-
-      expect(spy).toHaveBeenCalledTimes(1);
-
-      if (inputType === 'checkbox') {
+      it('should change the `checked` state from `false` to `true`', () => {
         expect(wrapper.state().checked).toBe(true);
-      } else {
+      });
+    });
+
+    describe('but the `hovered` prop has changed from `false` to `true`', () => {
+      beforeEach(() => {
+        wrapper.setProps({ hovered: true });
+      });
+
+      it('should change the `hovered` state `false` => `true`', () => {
+        expect(wrapper.state().hovered).toBe(true);
+      });
+    });
+
+    describe('and the `<ins />` is clicked', () => {
+      beforeEach(() => {
+        wrapper.find('ins').simulate('click');
+      });
+
+      it('should have the `checked` state as `false`', () => {
         expect(wrapper.state().checked).toBe(false);
-      }
+      });
+    });
+  });
+
+  describe('and the `insert` prop is `<div>test</div>} />`', () => {
+    describe('as render', () => {
+      beforeEach(() => {
+        wrapper = render(
+          <Input inputType={inputType} insert={<div>test</div>} />,
+        );
+      });
+
+      it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('and the `aria` prop is `true`', () => {
+    describe('as shallow', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Input inputType={inputType} aria />);
+      });
+
+      it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+
+      it('should have the `aria-checked` prop as `false`', () => {
+        expect(wrapper.props()['aria-checked']).toBe(false);
+      });
+    });
+  });
+
+  describe('and the `className` prop is `test`', () => {
+    describe('and the `inheritClassName` is `true`', () => {
+      describe('as shallow', () => {
+        beforeEach(() => {
+          wrapper = shallow(
+            <Input className="test" inputType={inputType} inheritClassName />,
+          );
+        });
+
+        it(`should have the \`className\` prop as \`i${inputType} test\``, () => {
+          expect(wrapper.props().className).toBe(`i${inputType} test`);
+        });
+      });
+    });
+  });
+
+  describe('and the `id` prop is `test`', () => {
+    describe('and the `inheritID` is `true`', () => {
+      describe('as shallow', () => {
+        beforeEach(() => {
+          wrapper = shallow(
+            <Input id="test" inputType={inputType} inheritID />,
+          );
+        });
+
+        it('should have the `id` prop as `iCheck-test`', () => {
+          expect(wrapper.props().id).toBe('iCheck-test');
+        });
+      });
+    });
+  });
+
+  describe('and the `isMobile` prop is `true`', () => {
+    describe('as shallow', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Input inputType={inputType} isMobile />);
+      });
+
+      describe('an `<input />`', () => {
+        beforeEach(() => {
+          wrapper = wrapper.find('input');
+        });
+
+        it('should match style attribute to the snapshot', () => {
+          expect(wrapper.prop('style')).toMatchSnapshot();
+        });
+      });
+    });
+  });
+
+  describe('and the `increaseArea` prop is `30%`', () => {
+    describe('as shallow', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Input increaseArea="30%" inputType={inputType} />);
+      });
+
+      it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('and the `increaseArea` prop is `-30%`', () => {
+    describe('as shallow', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Input increaseArea="-30%" inputType={inputType} />);
+      });
+
+      it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('and the `increaseArea` prop is `-60%`', () => {
+    describe('as shallow', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Input increaseArea="-60%" inputType={inputType} />);
+      });
+
+      it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('and the `onFocus` prop is passed', () => {
+    let onFocus;
+
+    beforeEach(() => {
+      onFocus = jest.fn();
     });
 
-    it('onClick() when both label and insert are not set', () => {
-      const wrapper = mount(testInput);
+    describe('as mount', () => {
+      beforeEach(() => {
+        wrapper = mount(<Input inputType={inputType} onFocus={onFocus} />);
+      });
 
-      expect(wrapper.state().checked).toBe(false);
-      wrapper.find('ins').simulate('click');
-      expect(wrapper.state().checked).toBe(false);
+      it('should not call the passed `onFocus` function', () => {
+        expect(onFocus).toHaveBeenCalledTimes(0);
+      });
+
+      it('should have the `focused` state as `false`', () => {
+        expect(wrapper.state().focused).toBe(false);
+      });
+
+      describe('and the `<input />` is focused', () => {
+        beforeEach(() => {
+          wrapper.find('input').simulate('focus');
+        });
+
+        it('should call the passed `onFocus` function', () => {
+          expect(onFocus).toHaveBeenCalledTimes(1);
+        });
+
+        it('should have the `focused` state as `true`', () => {
+          expect(wrapper.state().focused).toBe(true);
+        });
+      });
+    });
+  });
+
+  describe('and the `onBlur` prop is passed', () => {
+    let onBlur;
+
+    beforeEach(() => {
+      onBlur = jest.fn();
     });
 
-    it('onClick() when disabled', () => {
-      const wrapper = mount(<Input inputType={inputType} disabled />);
+    describe('as mount', () => {
+      beforeEach(() => {
+        wrapper = mount(<Input inputType={inputType} onBlur={onBlur} />);
+      });
 
-      expect(wrapper.state().checked).toBe(false);
-      wrapper.find('ins').simulate('click');
-      expect(wrapper.state().checked).toBe(false);
+      describe('and with the `focused` state as `true`', () => {
+        beforeEach(() => {
+          wrapper.setState({ focused: true });
+        });
+
+        it('should not call the passed `onBlur` function', () => {
+          expect(onBlur).toHaveBeenCalledTimes(0);
+        });
+
+        it('should have the `focused` state as `true`', () => {
+          expect(wrapper.state().focused).toBe(true);
+        });
+
+        describe('and the `<input />` is blurred', () => {
+          beforeEach(() => {
+            wrapper.find('input').simulate('blur');
+          });
+
+          it('should call the passed `onBlur` function', () => {
+            expect(onBlur).toHaveBeenCalledTimes(1);
+          });
+
+          it('should have the `focused` state as `false`', () => {
+            expect(wrapper.state().focused).toBe(false);
+          });
+        });
+      });
+    });
+  });
+
+  describe('and the `onChange` prop is passed', () => {
+    let onChange;
+
+    beforeEach(() => {
+      onChange = jest.fn();
+    });
+
+    describe('as mount', () => {
+      beforeEach(() => {
+        wrapper = mount(<Input inputType={inputType} onChange={onChange} />);
+      });
+
+      it('should not call the passed `onChange` function', () => {
+        expect(onChange).toHaveBeenCalledTimes(0);
+      });
+
+      it('should have the `checked` state as `false`', () => {
+        expect(wrapper.state().checked).toBe(false);
+      });
+
+      describe('and the `<input />` is changed', () => {
+        beforeEach(() => {
+          wrapper.find('input').simulate('change');
+        });
+
+        it('should call the passed `onChange` function', () => {
+          expect(onChange).toHaveBeenCalledTimes(1);
+        });
+
+        it('should have the `checked` state as `true`', () => {
+          expect(wrapper.state().checked).toBe(true);
+        });
+      });
+    });
+
+    describe('and the `insert` prop is `Insert`', () => {
+      describe('and the `label` prop is `Label`', () => {
+        beforeEach(() => {
+          wrapper = mount(
+            <Input
+              inputType={inputType}
+              insert="Insert"
+              label="Label"
+              onChange={onChange}
+            />,
+          );
+        });
+
+        it('should not call the passed `onChange` function', () => {
+          expect(onChange).toHaveBeenCalledTimes(0);
+        });
+
+        it('should have the `checked` state as `false`', () => {
+          expect(wrapper.state().checked).toBe(false);
+        });
+
+        describe('and the `<ins />` is clicked', () => {
+          beforeEach(() => {
+            wrapper.find('ins').simulate('click');
+          });
+
+          it('should call the passed `onChange` function', () => {
+            expect(onChange).toHaveBeenCalledTimes(1);
+          });
+
+          const state = inputType === 'checkbox';
+          it(`should have the \`checked\` state as \`${state}\``, () => {
+            expect(wrapper.state().checked).toBe(state);
+          });
+        });
+      });
+    });
+  });
+
+  describe('and the `disabled` prop is `true`', () => {
+    beforeEach(() => {
+      wrapper = mount(<Input inputType={inputType} disabled />);
+    });
+
+    describe('and the `<ins />` is clicked', () => {
+      beforeEach(() => {
+        wrapper.find('ins').simulate('click');
+      });
+
+      it('should have the `checked` state as `false`', () => {
+        expect(wrapper.state().checked).toBe(false);
+      });
     });
   });
 };
 
-describe('<Input /> component', () => {
-  describe('with the prop inputType set to "checkbox"', () => {
+describe('Input component', () => {
+  describe('when the required `inputType` prop is set to `checkbox`', () => {
     testInputComponent('checkbox');
   });
 
-  describe('with the prop inputType set to "radio"', () => {
+  describe('when the required `inputType` prop is set to `radio`', () => {
     testInputComponent('radio');
   });
 });
