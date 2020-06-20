65ec705c3405a22a19ab8134bb3021f9b23cf807
Victor Popkov <victor@popkov.me>
2020-06-20 20:15:37 +0300

    Refactor RadioGroup tests

--- src/__tests__/RadioGroup.test.jsx
+++ src/__tests__/RadioGroup.test.jsx
@@ -3,80 +3,139 @@ import expect from 'expect';
 import { mount } from 'enzyme';
 import { Radio, RadioGroup } from '../index';
 
-describe('<RadioGroup /> component', () => {
-  const testRadioGroup = (
-    <RadioGroup name="test">
-      <Radio />
-    </RadioGroup>
-  );
+describe('RadioGroup component', () => {
+  let wrapper;
 
-  it('should have the default props values when mounted', () => {
-    const wrapper = mount(testRadioGroup);
-    expect(wrapper.props()).toMatchSnapshot();
-  });
-
-  describe('should render the corresponding HTML', () => {
-    it('when only the default props are set', () => {
-      const wrapper = mount(testRadioGroup);
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('when the radioWrapClassName and radioWrapTag props values are set', () => {
-      const wrapper = mount(
-        <RadioGroup name="test" radioWrapClassName="test" radioWrapTag="div">
-          <Radio />
-        </RadioGroup>,
-      );
-
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-  describe('should update the state', () => {
-    it('when the value prop has changed', () => {
-      const spy = jest.spyOn(Radio.prototype, 'componentWillReceiveProps');
-      const wrapper = mount(testRadioGroup);
-
-      expect(spy).toHaveBeenCalledTimes(0);
-      expect(wrapper.props().value).toBe(null);
-      expect(wrapper.state().value).toBe('');
-
-      wrapper.setProps({ value: 'test' });
-
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(wrapper.props().value).toBe('test');
-      expect(wrapper.state().value).toBe('test');
-    });
-
-    describe('when handling the <input />', () => {
-      it('onChange()', () => {
-        const spy = jest.fn();
-        const wrapper = mount(
-          <RadioGroup
-            name="test"
-            radioWrapClassName="test"
-            radioWrapTag="div"
-            onChange={spy}
-          >
+  describe('when the required `name` prop is set to `test`', () => {
+    describe('as mount', () => {
+      beforeEach(() => {
+        wrapper = mount(
+          <RadioGroup name="test">
             <Radio value="1" />
             <Radio value="2" />
           </RadioGroup>,
         );
+      });
 
+      it('should match the snapshot', () => {
         expect(wrapper).toMatchSnapshot();
+      });
 
-        expect(spy).toHaveBeenCalledTimes(0);
+      it('should match the default props snapshot', () => {
+        expect(wrapper.props()).toMatchSnapshot();
+      });
+
+      it('should match the default state snapshot', () => {
+        expect(wrapper.state()).toMatchSnapshot();
+      });
+
+      it('should have the `value` prop as `null`', () => {
+        expect(wrapper.props().value).toBe(null);
+      });
+
+      it('should have the `value` state as ``', () => {
         expect(wrapper.state().value).toBe('');
+      });
 
-        wrapper.find('input').first().simulate('change');
+      describe('and the `value` prop has changed from `` to `test`', () => {
+        beforeEach(() => {
+          wrapper.setProps({ value: 'test' });
+        });
 
-        expect(spy).toHaveBeenCalledTimes(1);
-        expect(wrapper.state().value).toBe('1');
+        it('should have the `checked` prop as `test`', () => {
+          expect(wrapper.props().value).toBe('test');
+        });
 
-        wrapper.find('input').last().simulate('change');
+        it('should have the `checked` state as `test`', () => {
+          expect(wrapper.state().value).toBe('test');
+        });
+      });
+    });
 
-        expect(spy).toHaveBeenCalledTimes(2);
-        expect(wrapper.state().value).toBe('2');
+    describe('and the `radioWrapClassName` prop is `test`', () => {
+      describe('and the `radioWrapTag` prop is `div`', () => {
+        describe('as mount', () => {
+          beforeEach(() => {
+            wrapper = mount(
+              <RadioGroup
+                name="test"
+                radioWrapClassName="test"
+                radioWrapTag="div"
+              >
+                <Radio value="1" />
+                <Radio value="2" />
+              </RadioGroup>,
+            );
+          });
+
+          it('should match the snapshot', () => {
+            expect(wrapper).toMatchSnapshot();
+          });
+        });
+
+        describe('and the `onChange` prop is passed', () => {
+          let onChange;
+
+          beforeEach(() => {
+            onChange = jest.fn();
+          });
+
+          describe('as mount', () => {
+            beforeEach(() => {
+              wrapper = mount(
+                <RadioGroup
+                  name="test"
+                  radioWrapClassName="test"
+                  radioWrapTag="div"
+                  onChange={onChange}
+                >
+                  <Radio value="1" />
+                  <Radio value="2" />
+                </RadioGroup>,
+              );
+            });
+
+            it('should match the snapshot', () => {
+              expect(wrapper).toMatchSnapshot();
+            });
+
+            it('should not call the passed `onChange` function', () => {
+              expect(onChange).toHaveBeenCalledTimes(0);
+            });
+
+            it('should have the `value` state as ``', () => {
+              expect(wrapper.state().value).toBe('');
+            });
+
+            describe('and the first `<input />` is changed', () => {
+              beforeEach(() => {
+                wrapper.find('input').first().simulate('change');
+              });
+
+              it('should call the passed `onChange` function', () => {
+                expect(onChange).toHaveBeenCalledTimes(1);
+              });
+
+              it('should have the `value` state as `1`', () => {
+                expect(wrapper.state().value).toBe('1');
+              });
+            });
+
+            describe('and the last `<input />` is changed', () => {
+              beforeEach(() => {
+                wrapper.find('input').last().simulate('change');
+              });
+
+              it('should call the passed `onChange` function', () => {
+                expect(onChange).toHaveBeenCalledTimes(1);
+              });
+
+              it('should have the `value` state as `2`', () => {
+                expect(wrapper.state().value).toBe('2');
+              });
+            });
+          });
+        });
       });
     });
   });
