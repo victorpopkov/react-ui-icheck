8506073844e9aee7d4f2d6d51745204085a4de02
Victor Popkov <victor@popkov.me>
2023-02-23 04:05:21 +0200

    Fix code style issues

--- .github/jest-output.js
+++ .github/jest-output.js
@@ -4,7 +4,7 @@ class JestOutput {
   // eslint-disable-next-line class-methods-use-this
   onRunComplete(testContexts, results) {
     const summary = results.coverageMap.getCoverageSummary();
-    core.setOutput('coverage', `${summary.lines.pct}%` || '0%')
+    core.setOutput('coverage', `${summary.lines.pct}%` || '0%');
     core.setOutput('passed', results.numPassedTests || 0);
     core.setOutput('total', results.numTotalTests || 0);
   }
