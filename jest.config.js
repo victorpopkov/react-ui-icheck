94424021b9addaa6a2f2c983b97afcb291178cef
Victor Popkov <victor@popkov.me>
2024-04-09 19:51:51 +0300

    Migrate from Enzyme to React Testing Library

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['<rootDir>/src/index.js'],
   modulePathIgnorePatterns: ['<rootDir>/.github/', '<rootDir>/lib/'],
-  setupFiles: ['<rootDir>/tests/shim.js', '<rootDir>/tests/setup.js'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
   testEnvironment: 'jsdom',
 };
--- tests/CheckboxGroup.spec.jsx
+++ tests/CheckboxGroup.spec.jsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import expect from 'expect';
 import { render } from '@testing-library/react';
