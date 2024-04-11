0cf7d1893925fdf62bcacac55a0fd7559c806031
Victor Popkov <victor@popkov.me>
2024-04-11 23:05:43 +0300

    Fix issue with getting the coverage summary for test job in ci GA workflow

--- .github/jest-output.js
+++ .github/jest-output.js
@@ -3,10 +3,11 @@ const core = require('@actions/core');
 class JestOutput {
   // eslint-disable-next-line class-methods-use-this
   onRunComplete(testContexts, results) {
-    const summary = results.coverageMap.getCoverageSummary();
-    core.setOutput('coverage', `${summary.lines.pct}%` || '0%');
-    core.setOutput('passed', results.numPassedTests || 0);
-    core.setOutput('total', results.numTotalTests || 0);
+    const totalTests = results?.numTotalTests || 0;
+    core.setOutput('total', totalTests);
+
+    const passedTests = results?.numPassedTests || 0;
+    core.setOutput('passed', passedTests);
   }
 }
 
